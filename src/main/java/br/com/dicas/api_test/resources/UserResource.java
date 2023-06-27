3f67dbed546df931ea91a1ae582206fb7c30eb5e
Marcos Alexandria <marcos.alexandria01@gmail.com>
2023-06-27 09:42:59 -0300

    Aula 07 Padrao DTO - [Testes com JUnit5 Mockito e Java]

--- src/main/java/br/com/dicas/api_test/resources/UserResource.java
+++ src/main/java/br/com/dicas/api_test/resources/UserResource.java
@@ -1,7 +1,8 @@
 package br.com.dicas.api_test.resources;
 
-import br.com.dicas.api_test.domain.User;
+import br.com.dicas.api_test.domain.dto.UserDTO;
 import br.com.dicas.api_test.services.imp.UserServiceImp;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
     @Autowired
+    ModelMapper mapper;
+    @Autowired
     UserServiceImp service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 }
